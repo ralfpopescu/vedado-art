ff759cd2937dfbde14af487bc30fb741a832ca18
Ralf Popescu <ralf@landinglion.com>
2019-07-01 19:54:32 -0400

    Updates

---
+++ precache-manifest.04704cf66598548adf94ff42883c862b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "db254ad04edf72f4610f",
+    "url": "/ralfpopescu/vedado-art/static/css/main.ff968f64.chunk.css"
+  },
+  {
+    "revision": "db254ad04edf72f4610f",
+    "url": "/ralfpopescu/vedado-art/static/js/main.db254ad0.chunk.js"
+  },
+  {
+    "revision": "95240dd143fb95fbf6bf",
+    "url": "/ralfpopescu/vedado-art/static/js/1.95240dd1.chunk.js"
+  },
+  {
+    "revision": "8aea6ba51de43d08cdc5",
+    "url": "/ralfpopescu/vedado-art/static/js/runtime~main.8aea6ba5.js"
+  },
+  {
+    "revision": "dd210b14fb33e3e1157448997adccb62",
+    "url": "/ralfpopescu/vedado-art/static/media/play-arrow.dd210b14.svg"
+  },
+  {
+    "revision": "2a7860b62b7f5e43cbf9a4a7d56ed91c",
+    "url": "/ralfpopescu/vedado-art/static/media/download.2a7860b6.svg"
+  },
+  {
+    "revision": "31c348eb1a3119d65fc3d02b92670ce9",
+    "url": "/ralfpopescu/vedado-art/index.html"
+  }
+];
